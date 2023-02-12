d6106cae4ec8d80e2e4ac190a00faff3ea8ef1fd
Jonathan Phair <jmphair@gmail.com>
2023-02-12 14:31:22 -0500

    add: getAllFoods query to customers file

---
+++ db/queries/customers.js
@@ -0,0 +1,11 @@
+/**
+ *
+ * User Story: 1
+ *
+ * As a customer, I want to see all the available items on the menu because I want to select menu items that I want to eat.
+ *
+ * Query (c): Select all the items  from the foods table.
+ * Then we render the data to the menu cards. → return array of data → ('api/customer/menu' : GET)
+ *
+ */
+
