bf74890ea63131c5db0656358075a0ff5cdcf07f
dorreb <ashley_ure@hotmail.com>
2023-02-11 21:30:58 -0500

    feature: adding seed files

---
+++ db/seeds/01_foods.sql
@@ -0,0 +1,266 @@
+-- Users foods seeds here
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'East Coast Oysters',
+    'Starters',
+    '',
+    'smoked buttermilk, ramp, lemon',
+    16,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'Beef Tartare',
+    'Starters',
+    '',
+    'egg yolk, sea asparagus, beef tendon chip',
+    19,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'Heirloom Beet Salad',
+    'Starters',
+    '',
+    'smoked sheep yogurt, umeboshi, rye crumble',
+    20,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'Aloette Burger',
+    'Mains',
+    '',
+    'beaufort cheese, onion, lettuce',
+    20,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    '12oz Prime Striploin',
+    'Mains',
+    '',
+    'sauce au poivre, smoked eggplant',
+    60,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'Charred Squid',
+    'Mains',
+    '',
+    'romanesco, nduja, meyer lemon',
+    28,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'Crispy Squash',
+    'Mains',
+    '',
+    'brown butter hollandaise, parmesan, pepita',
+    16,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'Calamarata Pasta',
+    'Mains',
+    '',
+    'vodka sauce, burrata, parsley',
+    34,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'Chocolate Caramel Tart',
+    'Desserts',
+    '',
+    'araguani 72% chocolate ganache, cocoa sable, caramel cremeux, orelys whipped ganache',
+    14,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'Lemon Meringue Pie',
+    'Desserts',
+    '',
+    'lemon crémeux, toasted meringue, almond salblé',
+    12,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'Apple Crumble',
+    'Desserts',
+    '',
+    'warm apple compote, cinnamon crumble, french vanilla ice cream',
+    14,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'East Coast Oysters',
+    'Starters',
+    '',
+    'smoked buttermilk, ramp, lemon',
+    16,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'East Coast Oysters',
+    'Starters',
+    '',
+    'smoked buttermilk, ramp, lemon',
+    16,
+    1
+  );
+
+INSERT INTO
+  foods (
+    name,
+    category,
+    photo_url,
+    description,
+    price,
+    restaurant_id
+  )
+VALUES
+  (
+    'East Coast Oysters',
+    'Starters',
+    '',
+    'smoked buttermilk, ramp, lemon',
+    16,
+    1
+  );
