264dc480bd279d3f4dadeb4e58819b4ddc9ff39d
dorreb <ashley_ure@hotmail.com>
2023-02-11 23:02:00 -0500

    feature: inputting seed data

--- db/seeds/01_foods.sql
+++ db/seeds/01_foods.sql
@@ -1,4 +1,4 @@
--- Users foods seeds here
+-- foods seeds here
 INSERT INTO
   foods (
     name,
@@ -12,7 +12,7 @@ VALUES
   (
     'East Coast Oysters',
     'Starters',
-    '',
+    'https://media-cdn.tripadvisor.com/media/photo-s/15/ea/9b/fd/oysters.jpg',
     'smoked buttermilk, ramp, lemon',
     16,
     1
@@ -31,7 +31,7 @@ VALUES
   (
     'Beef Tartare',
     'Starters',
-    '',
+    'https://cdn.tasteatlas.com/images/dishes/baab0ab2a0b041f883f01e68ac9f1ba5.jpg',
     'egg yolk, sea asparagus, beef tendon chip',
     19,
     1
@@ -50,7 +50,7 @@ VALUES
   (
     'Heirloom Beet Salad',
     'Starters',
-    '',
+    'https://www.petrossian.com/blog/wp-content/uploads/2019/11/Picture2.jpg',
     'smoked sheep yogurt, umeboshi, rye crumble',
     20,
     1
@@ -69,7 +69,7 @@ VALUES
   (
     'Aloette Burger',
     'Mains',
-    '',
+    'https://aloetterestaurant.com/wp-content/uploads/2017/10/food-image-6.jpg',
     'beaufort cheese, onion, lettuce',
     20,
     1
@@ -88,8 +88,8 @@ VALUES
   (
     '12oz Prime Striploin',
     'Mains',
-    '',
-    'sauce au poivre, smoked eggplant',
+    'https://aloetterestaurant.com/wp-content/uploads/2017/10/AE-Short-Rib-.jpg',
+    'sauce au poivre',
     60,
     1
   );
@@ -105,11 +105,11 @@ INSERT INTO
   )
 VALUES
   (
-    'Charred Squid',
+    'Seabass',
     'Mains',
-    '',
-    'romanesco, nduja, meyer lemon',
-    28,
+    'https://i.pinimg.com/originals/36/1e/4f/361e4f633c5d85e9eb24c8343352c1e4.jpg',
+    'mint, potato cream, meyer lemon',
+    32,
     1
   );
 
@@ -124,9 +124,9 @@ INSERT INTO
   )
 VALUES
   (
-    'Crispy Squash',
+    'Squash Carpaccio',
     'Mains',
-    '',
+    'https://media-cdn.tripadvisor.com/media/photo-s/12/99/38/5a/butternut-squash-carpaccio.jpg',
     'brown butter hollandaise, parmesan, pepita',
     16,
     1
@@ -143,9 +143,9 @@ INSERT INTO
   )
 VALUES
   (
-    'Calamarata Pasta',
+    'Pasta',
     'Mains',
-    '',
+    'https://www.billyparisi.com/wp-content/uploads/2019/09/pomodoro-fb.jpg',
     'vodka sauce, burrata, parsley',
     34,
     1
@@ -164,7 +164,7 @@ VALUES
   (
     'Chocolate Caramel Tart',
     'Desserts',
-    '',
+    'https://i.pinimg.com/736x/eb/0d/75/eb0d7584a3e5671aa917c5ac635483bf.jpg',
     'araguani 72% chocolate ganache, cocoa sable, caramel cremeux, orelys whipped ganache',
     14,
     1
@@ -183,7 +183,7 @@ VALUES
   (
     'Lemon Meringue Pie',
     'Desserts',
-    '',
+    'https://s3-eu-west-1.amazonaws.com/onin.london/assets/Deconstructed-Lemon-Meringue-Pie-1024x682.jpg',
     'lemon crémeux, toasted meringue, almond salblé',
     12,
     1
@@ -202,7 +202,7 @@ VALUES
   (
     'Apple Crumble',
     'Desserts',
-    '',
+    'https://aloetterestaurant.com/wp-content/uploads/2017/10/Aloette-2022-Action-Shots-21-1.jpg',
     'warm apple compote, cinnamon crumble, french vanilla ice cream',
     14,
     1
@@ -219,11 +219,11 @@ INSERT INTO
   )
 VALUES
   (
-    'East Coast Oysters',
-    'Starters',
-    '',
-    'smoked buttermilk, ramp, lemon',
-    16,
+    'Espresso Martini',
+    'Drinks',
+    'https://hips.hearstapps.com/hmg-prod/images/210923-delish-seo-espresso-martini-horizontal-0087-eb-1633535263.jpg?crop=0.8893229166666666xw:1xh;center,top&resize=1200:*',
+    'kraken, kahlúa, tonka, espresso',
+    20,
     1
   );
 
@@ -238,11 +238,11 @@ INSERT INTO
   )
 VALUES
   (
-    'East Coast Oysters',
-    'Starters',
-    '',
-    'smoked buttermilk, ramp, lemon',
-    16,
+    'French 55',
+    'Drinks',
+    'https://www.acouplecooks.com/wp-content/uploads/2019/12/French-75-001.jpg',
+    'botanist gin, lavender, lemon, champagne',
+    32,
     1
   );
 
@@ -257,10 +257,10 @@ INSERT INTO
   )
 VALUES
   (
-    'East Coast Oysters',
-    'Starters',
-    '',
-    'smoked buttermilk, ramp, lemon',
-    16,
+    'Nogroni',
+    'Drinks',
+    'https://res.cloudinary.com/sagacity/image/upload/c_crop,h_4576,w_4576,x_0,y_0/c_limit,dpr_auto,f_auto,fl_lossy,q_80,w_1080/shutterstock_1478698988_pwhqh1.png',
+    'gin, orange bitter, vermouth',
+    12,
     1
   );
---
+++ db/seeds/02_restaurants.sql
@@ -0,0 +1,21 @@
+-- restaurants seeds here
+INSERT INTO
+  restaurants (
+    id,
+    name,
+    address,
+    phone_number,
+    photo_url,
+    username,
+    password
+  )
+VALUES
+  (
+    1,
+    'Aloette',
+    '163 Spadina Ave. Toronto, ON M5V 2L6',
+    '416.260.3444',
+    'https://aloetterestaurant.com/wp-content/uploads/2017/10/Aloette-Interiors_003.jpg',
+    'admin',
+    'admin'
+  );
