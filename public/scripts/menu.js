824a17696b43b797554e1b8ffecfef06d7a0889f
Jonathan Phair <jmphair@gmail.com>
2023-02-13 22:55:44 -0500

    add: renderCartContents draft and temp cart object

--- public/scripts/menu.js
+++ public/scripts/menu.js
@@ -10,11 +10,27 @@ $(() => {
     success: renderAllMenuCards,
   });
 
+  const cartArray = [];
+
+  $(document).on("submit", "form.add-to-cart", (e) => {
+    e.preventDefault()
+    const target = e.target;
+    const formData = $(target).serialize();
+    const formDataArray = formData.replace(/%20/g, " ").split("&");
+    const finalDataArray = formDataArray.map(data => data.split("="));
+    const result = {};
+    finalDataArray.forEach(data => {
+      result[data[0]] = data[1];
+    })
+    cartArray.push(result);
+    renderCartContents(cartArray);
+  })
 });
 
 
 const createMenuCard = (name, photo_url, description, price) => {
-  let $menuCard = $(`
+  // const menu_item = { name, price };
+  const $menuCard = $(`
   <div class="menu-item card horizontal">
     <div class="card-image">
       <img
@@ -28,7 +44,11 @@ const createMenuCard = (name, photo_url, description, price) => {
         <p>${price}</p>
       </div>
       <div class="card-action">
-        <a href="#">Add to cart</a>
+        <form class="add-to-cart">
+        <input type="hidden" name="menu_item_name" value="${name}"/>
+        <input type="hidden" name="menu_item_price" value="${price}"/>
+        <button class="add-on-click" type="submit">Add to cart</button>
+        </form>
       </div>
     </div>
   </div>
@@ -64,6 +84,15 @@ const renderAllMenuCards = (foods) => {
 }
 
 
+
+
 const renderCartContents = (customer_order) => {
+  // $(".menu-item-container").append(`
+  // <div class="menu-item">
+  // <p>${} x ${}</p>
+  // <p>${}</p>
+  // </div>
+  // `);
+  console.log(customer_order)
 
 }
