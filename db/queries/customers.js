dfdeeb65c475c212fb4d0c362ede92bd838460d1
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-02-15 20:15:18 +0000

    Status page and fix date bug in order and add cancel tab

--- db/queries/customers.js
+++ db/queries/customers.js
@@ -3,23 +3,25 @@ const db = require("../connection");
 ///// This query adds a new food order into the food_orders bridge table
 
 const addFoodOrder = (foodArray, order_id) => {
-    const valuesArray = foodArray.map((id) => {
-      return `(${id}, $1)`
-    })
-    const valueString = valuesArray.join(', ')
-    return db.query(
+  const valuesArray = foodArray.map((id) => {
+    return `(${id}, $1)`;
+  });
+  const valueString = valuesArray.join(", ");
+  return db
+    .query(
       `INSERT INTO food_orders (food_id, order_id)
        VALUES ${valueString}
        RETURNING *;
       `,
       [order_id]
     )
-    .then((data) => {return data.rows})
+    .then((data) => {
+      return data.rows;
+    })
     .catch((err) => {
       console.log(err.message);
     });
 };
-
 
 ///// This query gets all the food item data from the foods table.
 
@@ -34,7 +36,6 @@ const getAllFoods = () => {
     });
 };
 
-
 ///// This query gets all the customers from the customers table.
 
 const getAllCustomers = () => {
@@ -48,46 +49,68 @@ const getAllCustomers = () => {
     });
 };
 
-
 // This query adds a new customer to the customers table.
 
 const addCustomer = (customers) => {
   return db
-  .query(`
+    .query(
+      `
     INSERT INTO customers (name, phone_number)
     VALUES ($1, $2)
     RETURNING *;
-  `, [customers[0], customers[1]])
-  .then((data) => {
-    return data.rows[0];
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
+  `,
+      [customers[0], customers[1]]
+    )
+    .then((data) => {
+      return data.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
-
 
 // This query adds a new customer order.
 
 const addOrder = (customer_id) => {
   return db
-  .query(`
+    .query(
+      `
     INSERT INTO orders (customer_id, created_at)
     VALUES ($1, NOW())
     RETURNING *;
-  `, [customer_id])
-  .then((data) => {
-    return data.rows[0];
-  })
-  .catch((err) => {
-    console.log(err.message);
-  });
-}
+  `,
+      [customer_id]
+    )
+    .then((data) => {
+      return data.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+const getOrderById = (order_id) => {
+  return db
+    .query(
+      `
+    SELECT * FROM orders
+    WHERE orders.id = $1;
+  `,
+      [order_id]
+    )
+    .then((data) => {
+      return data.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
 
 module.exports = {
   getAllFoods,
   addFoodOrder,
   getAllCustomers,
   addCustomer,
-  addOrder
+  addOrder,
+  getOrderById,
 };
