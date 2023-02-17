cbaef75f5bbad39f98d60c82c80457ecaa91b607
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-02-17 18:15:17 +0000

    Change DATABASE URL back to DB_HOST

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ const { Pool } = require("pg");
 
 /* Comment dbParams below to run in your own database */
 const dbParams = {
-  host: process.env.DATABASE_URL,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
