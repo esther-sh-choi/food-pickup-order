846b93934522ae8abf97c917bcff18e616f0fbce
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-02-17 04:48:13 +0000

    Enable heroku

--- db/connection.js
+++ db/connection.js
@@ -2,21 +2,21 @@
 const { Pool } = require("pg");
 
 /* Uncomment dbParams below to run in your own database and localhost */
-const dbParams = {
-  host: process.env.DATABASE_URL,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-};
+// const dbParams = {
+//   host: process.env.DATABASE_URL,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+// };
 
 /* Comment dbParams below to run in your own database */
-// const dbParams = {
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false,
-//   },
-// };
+const dbParams = {
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
 
 const db = new Pool(dbParams);
 
