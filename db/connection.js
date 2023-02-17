662979cde6a8d9689fe68426e728d57ea0d396f7
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-02-17 04:07:28 +0000

    change client

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,22 @@
 // PG database client/connection setup
-const { Pool } = require("pg");
+const { Pool, Client } = require("pg");
 
-const dbParams = {
-  host: process.env.DATABASE_URL,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-};
+// const dbParams = {
+//   host: process.env.DATABASE_URL,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+// };
 
-const db = new Pool(dbParams);
+// const db = new Pool(dbParams);
+
+const db = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
 
 db.connect();
 
