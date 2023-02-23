1a8f58dda598f80308afb4e8208cc2f25c50ad84
So Hyun (Esther) Choi <estherchoi.sh@gmail.com>
2023-02-23 04:06:09 +0000

    Change Twilio and login form layout

--- routes/restaurant-api.js
+++ routes/restaurant-api.js
@@ -40,9 +40,10 @@ router.post("/orders/:order_id/confirm", (req, res) => {
         const photo_url =
           "https://images-ext-1.discordapp.net/external/cU3k6BlDpujtHPW-Yk-cJYdC0kydqJeW5_Q4LCvrW6Q/https/torontolife.com/wp-content/uploads/2021/01/KRISS_FINAL04.jpg?width=999&height=666";
 
-        twilio
-          .smsMsgCustomer(customerName, message, phoneNumber, photo_url)
-          .then((res) => console.log(res));
+        // twilio
+        //   .smsMsgCustomer(customerName, message, phoneNumber, photo_url)
+        //   .then((res) => console.log(res));
+
         res.json(order);
       })
       .catch((e) => {
